8ec181c53b665b61949db6cb28065a45edfc2eaa
TheEggHealer <TheEggKiller@gmail.com>
2019-05-21 18:00:06 +0200

    removed gap

--- src/systems/chunk_generator_system.rs
+++ src/systems/chunk_generator_system.rs
@@ -67,8 +67,10 @@ impl<'s> System<'s> for ChunkGenerator {
                             sprite_sheet: resources.world_sprites.clone(),
                             sprite_number: sprite_id,
                         };
-
-                        transform.set_translation_xyz(Float::from(chunk_comp.x as f32 * 512. + x as f32 * 33.), Float::from(chunk_comp.y as f32 * 512. + y as f32 * 33.), 0.);
+                        
+                        let tile_x = chunk_comp.x * 512 + x * 32;
+                        let tile_y = chunk_comp.y * 512 + y * 32;
+                        transform.set_translation_xyz(Float::from(tile_x as f32), Float::from(tile_y as f32), 0.);
 
                         entities.build_entity()
                             .with(transform, &mut transforms)
