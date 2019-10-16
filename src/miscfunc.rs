5cb19e4b206f641b120a541d73d2153fdbb439b0
Alve™ <lolzman12@hotmail.se>
2019-10-16 21:55:47 +0200

    refactor & upgrade to amethyst 0.13.2

---
+++ src/miscfunc.rs
@@ -0,0 +1,27 @@
+use amethyst::{
+    assets::{AssetStorage, Loader, Handle},
+    core::transform::Transform,
+    ecs::prelude::{Component, DenseVecStorage},
+    prelude::*,
+    renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
+    window::ScreenDimensions,
+};
+
+
+pub const ARENA_HEIGHT: f32 = 100.0;
+pub const ARENA_WIDTH: f32 = 100.0;
+
+
+pub fn init_camera(world: &mut World, dimensions: &ScreenDimensions) {
+    // Center the camera in the middle of the screen, and let it cover
+    // the entire screen
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(dimensions.width() * 0.5, dimensions.height() * 0.5, 1.);
+
+    world
+        .create_entity()
+        .with(Camera::standard_2d(dimensions.width(), dimensions.height()))
+        .with(transform)
+        .build();
+}
+
--- src/zombie_curtains.rs
+++ src/zombie_curtains.rs
@@ -1,351 +1,48 @@
-use crate::systems::{entities::*, fps_system::Text, *};
-use crate::wire::particles::{EmitterTracker, ParticleEmitter};
-use crate::wire::ui::core::wire_create_basic_block;
-use amethyst::utils::removal::Removal;
-
 use amethyst::{
-    assets::{AssetStorage, Loader},
-    core::transform::Transform,
-    core::timing::Time,
-
-    ecs::prelude::*,
-    input::{is_close_requested, is_key_down},
     prelude::*,
-    renderer::{
-        camera::{Camera, Projection},
-        formats::texture::ImageFormat,
-        sprite::{SpriteRender, SpriteSheet, SpriteSheetFormat, SpriteSheetHandle},
-        Texture,
-    },
-    ui::*,
-    ui::{Anchor, TtfFormat, UiText, UiTransform},
-    utils::{application_dir, ortho_camera::*},
-    winit::VirtualKeyCode,
+    input::{get_key, is_close_requested, is_key_down, VirtualKeyCode},
+    window::ScreenDimensions,
 };
+
+use crate::miscfunc;
+
 
 pub struct ZombieCurtains;
 
-// TODO: A multi state world resources loading is needed!
-pub struct WorldResources {
-    pub world_sprites: Vec<SpriteSheetHandle>,
-    pub entity_sprites: Vec<SpriteSheetHandle>,
-}
-
-pub const CAMERA_ZOOM: f32 = 2.5;
-pub const CAMERA_SCALE_HEIGHT: f32 = 1080. / CAMERA_ZOOM;
-pub const CAMERA_SCALE_WIDTH: f32 = 1920. / CAMERA_ZOOM;
-
 impl SimpleState for ZombieCurtains {
+
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
-        world.register::<Removal<i16>>();
+        
+        let dimensions = (*world.read_resource::<ScreenDimensions>()).clone();
 
-        init_camera(world);
-        initialize_debugui(world);
-
-
-/*
-        wire_create_basic_block(world, [0.0, 0.0, 0.0, 1.0], UiTransform::new(
-            "we".to_string(),
-            Anchor::Middle,
-            Anchor::Middle,
-            104.,
-            -10.,
-            0.,
-            200.,
-            50.,
-        ), 0 );
-*/
-
-        let world_sprites = vec![
-            load_sprite(
-                world,
-                application_dir("resources")
-                    .unwrap()
-                    .join("textures")
-                    .join("grass")
-                    .to_string_lossy()
-                    .to_string(),
-                "tile".to_string(),
-            ),
-            load_sprite(
-                world,
-                application_dir("resources")
-                    .unwrap()
-                    .join("textures")
-                    .join("flower")
-                    .to_string_lossy()
-                    .to_string(),
-                "tile".to_string(),
-            ),
-            load_sprite(
-                world,
-                application_dir("resources")
-                    .unwrap()
-                    .join("textures")
-                    .join("particle")
-                    .to_string_lossy()
-                    .to_string(),
-                "tile".to_string(),
-            ),
-        ];
-
-        let entity_sprites = vec![load_sprite(
-            world,
-            application_dir("resources")
-                .unwrap()
-                .join("textures")
-                .join("wab_player")
-                .to_string_lossy()
-                .to_string(),
-            "player".to_string(),
-        )];
-        let grass = SpriteRender {
-            sprite_sheet: world_sprites[0].clone(),
-            sprite_number: 0,
-        };
-
-        let sprite = SpriteRender {
-            sprite_sheet: entity_sprites[0].clone(),
-            sprite_number: 0,
-        };
-
-        world.add_resource(WorldResources {
-            world_sprites: world_sprites.clone(),
-            entity_sprites: entity_sprites,
-        });
-
-        /*
-        for y in 0..32 {
-            for x in 0..48 {
-                let mut transform = Transform::default();
-                transform.set_translation_xyz(x as f32 * 32., y as f32 * 32., 0.);
-                world
-                    .create_entity()
-                    .with(transform)
-                    .with(grass.clone())
-                    .build();
-
-                world
-                    .create_entity()
-                    .with(GenerateChunk::new((x, y)))
-                    .build();
-            }
-        }
-        */
+        miscfunc::init_camera(world, &dimensions);
 
 
-        world
-            .create_entity()
-            .with(PlayerMovement::new())
-            .with(Transform::default())
-            .with(ParticleEmitter::new())
-            .with(sprite)
-            .build();
-
-        world.create_entity().with(Transform::default()).build();
     }
 
     fn handle_event(
         &mut self,
-        _: StateData<'_, GameData<'_, '_>>,
+        mut _data: StateData<'_, GameData<'_, '_>>,
         event: StateEvent,
     ) -> SimpleTrans {
-        match &event {
-            StateEvent::Window(event) => {
-                if is_close_requested(&event) || is_key_down(&event, VirtualKeyCode::Escape) {
-                    Trans::Quit
-                } else {
-                    Trans::None
-                }
+        if let StateEvent::Window(event) = &event {
+            // Check if the window should be closed
+            if is_close_requested(&event) || is_key_down(&event, VirtualKeyCode::Escape) {
+                return Trans::Quit;
             }
-            StateEvent::Ui(_ui_event) => Trans::None,
-            StateEvent::Input(_input) => Trans::None,
+
+            // Listen to any key events
+            if let Some(event) = get_key(&event) {
+                //info!("handling key event: {:?}", event);
+            }
+
+            // If you're looking for a more sophisticated event handling solution,
+            // including key bindings and gamepad support, please have a look at
+            // https://book.amethyst.rs/stable/pong-tutorial/pong-tutorial-03.html#capturing-user-input
         }
+
+        // Keep going
+        Trans::None
     }
-}
-
-
-/// Initialises a ui scoreboard
-fn initialize_debugui(world: &mut World) {
-    // Create a simple initial interface for rendering ui
-
-    let font = world.read_resource::<Loader>().load(
-        application_dir("resources")
-            .unwrap()
-            .join("font")
-            .join("MontserratSB.ttf")
-            .to_string_lossy()
-            .to_string(),
-        TtfFormat,
-        (),
-        &world.read_resource(),
-    );
-
-    let engine = UiTransform::new(
-        "we".to_string(),
-        Anchor::TopLeft,
-        Anchor::Middle,
-        104.,
-        -10.,
-        1.,
-        200.,
-        50.,
-    );
-    let p1_transform = UiTransform::new(
-        "P1".to_string(),
-        Anchor::TopLeft,
-        Anchor::Middle,
-        104.,
-        -25.,
-        1.,
-        200.,
-        50.,
-    );
-
-    let dt_transform = UiTransform::new(
-        "dt".to_string(),
-        Anchor::TopLeft,
-        Anchor::Middle,
-        104.,
-        -40.,
-        1.,
-        200.,
-        50.,
-    );
-
-    let mut font = UiText::new(
-        font.clone(),
-        "Debug info".to_string(),
-        [1., 1., 1., 1.],
-        15.,
-    );
-
-    font.align = Anchor::MiddleLeft;
-
-    world
-        .create_entity()
-        .with(engine)
-        .with(font.clone())
-        .build();
-
-    font.color = [0., 1., 0., 1.];
-    let fps = world
-        .create_entity()
-        .with(p1_transform)
-        .with(font.clone())
-        .build();
-
-    font.color = [1., 0.6, 0.0, 0.8];
-
-    let dt = world.create_entity().with(dt_transform).with(font).build();
-
-    world.add_resource(Text { fps });
-}
-
-fn init_camera(world: &mut World) {
-    let mut transform = Transform::default();
-    let mut camera_ortho = CameraOrtho::normalized(CameraNormalizeMode::Contain);
-    camera_ortho.world_coordinates = CameraOrthoWorldCoordinates {
-        left: -CAMERA_SCALE_WIDTH / 2.,
-        right: CAMERA_SCALE_WIDTH / 2.,
-        bottom: -CAMERA_SCALE_HEIGHT / 2.,
-        top: CAMERA_SCALE_HEIGHT / 2.,
-    };
-
-    transform.set_translation_z(2.0);
-
-    use crate::wire::{MoveComp, VelSlideComp, ZoomComp};
-    // Editor movement
-
-    world
-        .create_entity()
-        .with(Camera::from(Projection::orthographic(
-            0.0,
-            crate::WIDTH * 0.32 * 1.5,
-            0.0,
-            crate::HEIGHT * 0.32 * 1.5,
-            0.0,
-            10.0,
-        )))
-        //       .with(MoveComp::new())
-        //       .with(VelSlideComp::new())
-        .with(transform)
-        .with(CameraMovement::new())
-        .with(GeneratorSource::new())
-        .with(ZoomComp::new())
-        .with(camera_ortho)
-        .build();
-}
-
-/*
-#[derive(Debug, Clone)]
-struct LoadedSpriteSheet {
-    sprite_sheet_handle: SpriteSheetHandle,
-    sprite_count: u32,
-    sprite_w: u32,
-    sprite_h: u32,
-}
-
-fn load_sprite_sheet(world: &mut World) -> LoadedSpriteSheet {
-    let loader = world.read_resource::<Loader>();
-    let texture_handle = {
-        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
-        loader.load(
-            "texture/arrow_semi_transparent.png",
-            ImageFormat::default(),
-            (),
-            &texture_storage,
-        )
-    };
-    let sprite_w = 32;
-    let sprite_h = 32;
-    let sprite_sheet_definition = SpriteSheetDefinition::new(sprite_w, sprite_h, 2, 6, false);
-
-    let sprite_sheet = sprite_sheet_loader::load(texture_handle, &sprite_sheet_definition);
-    let sprite_count = sprite_sheet.sprites.len() as u32;
-
-    let sprite_sheet_handle = {
-        let loader = world.read_resource::<Loader>();
-        loader.load_from_data(
-            sprite_sheet,
-            (),
-            &world.read_resource::<AssetStorage<SpriteSheet>>(),
-        )
-    };
-
-    LoadedSpriteSheet {
-        sprite_sheet_handle,
-        sprite_count,
-        sprite_w,
-        sprite_h,
-    }
-}
-*/
-
-fn load_sprite(world: &mut World, path: String, ron: String) -> SpriteSheetHandle {
-    let texture_handle = {
-        let loader = world.read_resource::<Loader>();
-        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
-        loader.load(
-            format!("{}.png", path),
-            ImageFormat::default(),
-            (),
-            &texture_storage,
-        )
-    };
-    let loader = world.read_resource::<Loader>();
-    let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
-
-    let ronpath = application_dir("resources")
-        .unwrap()
-        .join("rons")
-        .join(format!("{}.ron", ron));
-
-    loader.load(
-        ronpath.to_string_lossy(),
-        SpriteSheetFormat(texture_handle),
-        (),
-        &sprite_sheet_store,
-    )
 }
